bd2310fbfe14ba14928b238de8d53e4ad617c350
SHAIKABDUL1186 <90095569+SHAIKABDUL1186@users.noreply.github.com>
2022-08-16 23:08:58 +0530

    Create execute.ps1

---
+++ execute.ps1
@@ -0,0 +1,10 @@
+Import-Module WebAdministration
+$AppPools = (Get-ChildItem -Path IIS:\AppPools\)
+
+
+
+foreach($AppPool in $AppPools)
+{
+Write-Host "Restarting Application Pool : $($AppPool.Name)"
+Restart-WebAppPool -Name $AppPool.Name
+}
